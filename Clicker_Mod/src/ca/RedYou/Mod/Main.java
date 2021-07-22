f9f0abc04d4bd699ae09ff4f198e6f72998320b3
RedYou <63940364+RedYou1@users.noreply.github.com>
2021-07-22 00:47:12 -0400

    pricing

--- Clicker_Mod/src/ca/RedYou/Mod/Main.java
+++ Clicker_Mod/src/ca/RedYou/Mod/Main.java
@@ -13,6 +13,12 @@ import ca.RedYou.Game.Controller.Player;
 
 public class Main extends Mod {
 
+	public Quantity getPrice(int id, Quantity amount) {
+		double a = (id + 9 + (id == 0 ? 6 : 0) + (id < 5 ? 0 : Math.pow(id - 5, 1.75))) * Math.pow(10, id);
+		double b = Math.pow(10, (Math.ceil(Math.log10(Math.ceil(a)))) / Math.log(10)) / 10;
+		return Quantity.valueOf((long) Math.ceil(Math.round(a / b) * b * Math.pow(1.15, amount.toLong())));
+	}
+
 	public Main(File gameFolder) {
 		super(gameFolder);
 
@@ -22,6 +28,8 @@ public class Main extends Mod {
 			ImageIcon farmPNG = new ImageIcon(ImageIO.read(getSource("farm.png")));
 
 			setEntity(new Entity() {
+
+				int restant = 0;
 
 				@Override
 				public String name() {
@@ -35,15 +43,18 @@ public class Main extends Mod {
 
 				@Override
 				public void action(Quantity quantity) {
-					Player.getInstance().getMoney().add(quantity);
+					Quantity q = new Quantity(quantity);
+					restant += q.div(Quantity.valueOf(10)).toLong();
+					if (restant >= 10) {
+						q.add(Quantity.valueOf(restant / 10));
+						restant = restant % 10;
+					}
+					Player.getInstance().getMoney().add(q);
 				}
 
 				@Override
 				public Quantity price(Quantity quantity) {
-					Quantity q = new Quantity(quantity);
-					q.add(Quantity.valueOf(1));
-					q.mult(Quantity.valueOf(2));
-					return q;
+					return getPrice(0, quantity);
 				}
 			});
 			setEntity(new Entity() {
@@ -60,17 +71,12 @@ public class Main extends Mod {
 
 				@Override
 				public void action(Quantity quantity) {
-					Quantity q = new Quantity(quantity);
-					q.mult(Quantity.valueOf(2));
-					Player.getInstance().getMoney().add(q);
+					Player.getInstance().getMoney().add(quantity);
 				}
 
 				@Override
 				public Quantity price(Quantity quantity) {
-					Quantity q = new Quantity(quantity);
-					q.add(Quantity.valueOf(1));
-					q.mult(Quantity.valueOf(3));
-					return q;
+					return getPrice(1, quantity);
 				}
 			});
 			setEntity(new Entity() {
@@ -88,16 +94,13 @@ public class Main extends Mod {
 				@Override
 				public void action(Quantity quantity) {
 					Quantity q = new Quantity(quantity);
-					q.mult(Quantity.valueOf(3));
+					q.mult(Quantity.valueOf(8));
 					Player.getInstance().getMoney().add(q);
 				}
 
 				@Override
 				public Quantity price(Quantity quantity) {
-					Quantity q = new Quantity(quantity);
-					q.add(Quantity.valueOf(1));
-					q.mult(Quantity.valueOf(4));
-					return q;
+					return getPrice(2, quantity);
 				}
 			});
 		} catch (IOException e) {
