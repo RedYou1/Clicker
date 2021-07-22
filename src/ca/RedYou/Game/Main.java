7696ba1843721335dd743e36c989b4c244992eb8
RedYou <63940364+RedYou1@users.noreply.github.com>
2021-07-21 21:20:45 -0400

    the game is back

--- src/ca/RedYou/Game/Main.java
+++ src/ca/RedYou/Game/Main.java
@@ -25,90 +25,81 @@ public class Main {
 	private static boolean running = true;
 
 	public static void main(String[] args) {
-		Quantity q = Quantity.valueOf(589);
-//		for (int i = 0; i < 100; i++) {
-		System.out.println(q.extract() + ":" + q);
-		Quantity mod = q.div(Quantity.valueOf(24));
-//		}
-		System.out.println(mod.extract() + ":" + mod);
-		System.out.println(q.extract() + ":" + q);
-//		ModController modCon = ModController.getInstance();
-//		File f = new File("Mods");
-//		if (f.exists() && f.isDirectory())
-//			for (File mod : f.listFiles()) {
-//				try {
-//					modCon.setMod(Mod.load(mod));
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//
-//		frame = new Frame();
-//
-//		quit.setBackground(Color.RED);
-//		quit.addActionListener(l -> {
-//			frame.quitter();
-//		});
-//
-//		click.addActionListener(l -> {
-//			Player.getInstance().getMoney().addOrSub(1);
-//			menu();
-//		});
-//		menu();
-//
-//		Thread t = new Thread("Action of Entities") {
-//			public void run() {
-//				while (running) {
-//					try {
-//						Player p = Player.getInstance();
-//						List<Entity> ents = new ArrayList<>(EntityController.getInstance().getEntities());
-//						for (Entity ent : ents) {
-//							ent.action(p.getEntityQuantity(ent));
-//						}
-//						money.setText(Player.getInstance().getMoney().toString());
-//						sleep(1000);
-//					} catch (Exception e) {
-//						e.printStackTrace();
-//					}
-//				}
-//			}
-//		};
-//		t.start();
+		ModController modCon = ModController.getInstance();
+		File f = new File("Mods");
+		if (f.exists() && f.isDirectory())
+			for (File mod : f.listFiles()) {
+				try {
+					modCon.setMod(Mod.load(mod));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+
+		frame = new Frame();
+
+		quit.setBackground(Color.RED);
+		quit.addActionListener(l -> {
+			frame.quitter();
+		});
+
+		click.addActionListener(l -> {
+			Player.getInstance().getMoney().add(Quantity.valueOf(1));
+			menu();
+		});
+		menu();
+
+		Thread t = new Thread("Action of Entities") {
+			public void run() {
+				while (running) {
+					try {
+						Player p = Player.getInstance();
+						List<Entity> ents = new ArrayList<>(EntityController.getInstance().getEntities());
+						for (Entity ent : ents) {
+							ent.action(p.getEntityQuantity(ent));
+						}
+						money.setText(Player.getInstance().getMoney().toString());
+						sleep(1000);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		};
+		t.start();
 	}
 
 	private static void menu() {
-//		frame.p.removeAll();
-//
-//		frame.p.setLabel(1, 0, Player.getInstance().getMoney().toString(), money);
-//		frame.p.setBouton("Cookie", 1, 1, click);
-//
-//		entites = frame.p.setSlider(2, 0, .25, .8, new Slider()).p;
-//
-//		Collection<Entity> Cents = EntityController.getInstance().getEntities();
-//		Entity[] ents = Cents.toArray(new Entity[Cents.size()]);
-//		for (int i = 0; i < ents.length; i++) {
-//			JButton b = new JButton();
-//			b.setIcon(ents[i].icon());
-//			final int a = i;
-//			b.addActionListener(k -> {
-//				if (Player.getInstance().getMoney()
-//						.compareTo(ents[a].price(Player.getInstance().getEntityQuantity(ents[a]))) > -1) {
-//					Quantity q = new Quantity(ents[a].price(Player.getInstance().getEntityQuantity(ents[a])));
-//					q.positif = false;
-//					Player.getInstance().getMoney().addOrSub(q);
-//
-//					q = Player.getInstance().getEntityQuantity(ents[a]);
-//					q.addOrSub(1);
-//
-//					menu();
-//				}
-//			});
-//			Quantity q = Player.getInstance().getEntityQuantity(ents[i]);
-//			entites.setBouton(
-//					"<html>" + q.toString() + " " + ents[i].name() + "<br>" + ents[i].price(q).toString() + "<html>", 0,
-//					i, b);
-//		}
-//
-//		frame.repaint();
+		frame.p.removeAll();
+
+		frame.p.setLabel(1, 0, Player.getInstance().getMoney().toString(), money);
+		frame.p.setBouton("Cookie", 1, 1, click);
+
+		entites = frame.p.setSlider(2, 0, .25, .8, new Slider()).p;
+
+		Collection<Entity> Cents = EntityController.getInstance().getEntities();
+		Entity[] ents = Cents.toArray(new Entity[Cents.size()]);
+		for (int i = 0; i < ents.length; i++) {
+			JButton b = new JButton();
+			b.setIcon(ents[i].icon());
+			final int a = i;
+			b.addActionListener(k -> {
+				if (Player.getInstance().getMoney()
+						.compareTo(ents[a].price(Player.getInstance().getEntityQuantity(ents[a]))) > -1) {
+					Player.getInstance().getMoney()
+							.sub(new Quantity(ents[a].price(Player.getInstance().getEntityQuantity(ents[a]))));
+
+					Player.getInstance().getEntityQuantity(ents[a]).add(Quantity.valueOf(1));
+
+					menu();
+				}
+			});
+			Quantity q = Player.getInstance().getEntityQuantity(ents[i]);
+			entites.setBouton(
+					"<html>" + q.toString() + " " + ents[i].name() + "<br>" + ents[i].price(q).toString() + "<html>", 0,
+					i, b);
+		}
+
+		frame.repaint();
 	}
 }
--- src/ca/RedYou/Game/Quantity.java
+++ src/ca/RedYou/Game/Quantity.java
@@ -1,6 +1,5 @@
 package ca.RedYou.Game;
 
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
